77bf39eb2c8e7918d6e52694a758f941728fcd54
Francisco Galea <galeafrancisco2001@gmail.com>
2024-10-02 00:05:43 -0300

    Changed access modifiers to private

--- Models/User.cs
+++ Models/User.cs
@@ -7,14 +7,45 @@ using System.Threading.Tasks;
 namespace SistemaReservas.Models
 {
     public class User
-    {  
-        private int UserId { get; set; }
-        private string UserName { get; set; }
-        private string Email { get; set; }
+    {
+        private int UserId;
+        private string UserName;
+        private string Email;
 
         public User(string userName, string email)
         {
             this.UserName = userName;
+            this.Email = email;
+        }
+
+        public User()
+        {
+        }
+        
+        public int GetUserId()
+        {
+            return this.UserId; 
+        }
+        public void SetUserId(int userId)
+        {
+            this.UserId = userId;
+        }
+
+        public string GetUserName()
+        {
+            return this.UserName;
+        }
+        public void SetUserName(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public string GetEmail()
+        {
+            return this.Email;
+        }
+        public void SetEmail(string email)
+        {
             this.Email = email;
         }
 
