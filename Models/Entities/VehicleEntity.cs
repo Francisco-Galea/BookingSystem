0856150213f8f34a552cc7ee79f43bc04c612f14
Francisco Galea <galeafrancisco2001@gmail.com>
2025-01-02 19:01:41 -0300

    Added new attributes

--- Models/Entities/VehicleEntity.cs
+++ Models/Entities/VehicleEntity.cs
@@ -3,12 +3,16 @@
     public class VehicleEntity : RentableEntity
     {
         private int VehicleId;
+        private string Brand;
+        private string Model;
         private int PassengerCapacity;
         private int SerialNumber;
 
-        public VehicleEntity(int PASSENGERCAPACITY, int SERIALNUMBER, string NAME, string DESCRIPTION, decimal MAINTENANCECOSTPERDAY, decimal COSTUSAGEPERDAY)
+        public VehicleEntity(string BRAND, string MODEL, int PASSENGERCAPACITY, int SERIALNUMBER, string NAME, string DESCRIPTION, decimal MAINTENANCECOSTPERDAY, decimal COSTUSAGEPERDAY)
         : base(NAME, DESCRIPTION, COSTUSAGEPERDAY)
         {
+            Brand = BRAND;
+            Model = MODEL;
             PassengerCapacity = PASSENGERCAPACITY;
             SerialNumber = SERIALNUMBER;
         }
@@ -17,6 +21,18 @@
         {
             get { return this.VehicleId; }
             set { this.VehicleId = value; }
+        }
+
+        private string BRAND
+        {
+            get { return this.Brand; }
+            set { this.Brand = value; }
+        }
+
+        private string MODEL
+        {
+            get { return this.Model; }
+            set { this.Model = value; }
         }
 
         public int PASSENGERCAPACITY
