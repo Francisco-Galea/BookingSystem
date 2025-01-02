39909601d9ac68a13de9fd0645c1c21eecf355f3
Francisco Galea <galeafrancisco2001@gmail.com>
2025-01-02 19:05:19 -0300

    Deleted attribute

--- Models/Entities/PropertyEntity.cs
+++ Models/Entities/PropertyEntity.cs
@@ -4,12 +4,10 @@
     {
         private int PropertyId;
         private string Location;
-        private int PassengerCapacity;
 
-        public PropertyEntity(string LOCATION, int PASSENGERCAPACITY, string NAME, string DESCRIPTION, decimal MAINTENANCECOSTPERDAY, decimal COSTUSAGERPERDAY) : base(NAME, DESCRIPTION, COSTUSAGERPERDAY)
+        public PropertyEntity(string LOCATION, string NAME, string DESCRIPTION, decimal MAINTENANCECOSTPERDAY, decimal COSTUSAGERPERDAY) : base(NAME, DESCRIPTION, COSTUSAGERPERDAY)
         {
             this.Location = LOCATION;
-            this.PassengerCapacity = PASSENGERCAPACITY;
         }
 
         public int PROPERTYID
@@ -30,26 +28,6 @@
                         throw new Exception("La ubicacion del la propiedad no debe estar vacia");
                     }
                     this.Location = value;
-                }
-                catch
-                {
-                    throw;
-                }
-            }
-        }
-
-        public int PASSENGERCAPACITY
-        {
-            get { return this.PassengerCapacity; }
-            set
-            {
-                try
-                {
-                    if (value <= 0)
-                    {
-                        throw new Exception("La capacidad de pasajeros no puede ser menor o igual a 0");
-                    }
-                    this.PassengerCapacity = value;
                 }
                 catch
                 {
