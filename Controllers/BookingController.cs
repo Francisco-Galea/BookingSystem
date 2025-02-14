46fd6d5aa2dd30e39d182b5dfc4bc52862bb06ce
Francisco Galea <galeafrancisco2001@gmail.com>
2025-02-14 16:56:09 -0300

    Starting to implement controllers

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -3,10 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Boocking.Views.ItemsView.RentableObjectsView;
+using Boocking.Views.RentableObjectsView;
+using Booking.Models.Entities;
 
 namespace Boocking.Controllers
 {
     public class BookingController
     {
+
+        public void CreateBooking(string selectedEntity)
+        {
+            try
+            {
+            
+            switch (selectedEntity)
+            {
+                case "Vehiculo":
+                    VehicleView vehicleView = new VehicleView();
+                    vehicleView.ShowDialog();
+                    break;
+
+                case "Propiedad":
+                    PropertyView propertyView = new PropertyView();
+                    propertyView.ShowDialog();
+                    break;
+
+                case "Dispositivo electronico":
+                    ElectronicView electronicView = new ElectronicView();
+                    electronicView.ShowDialog();
+                    break;
+
+                case "Indumentaria":
+                    IndumentaryView indumentaryView = new IndumentaryView();
+                    indumentaryView.ShowDialog();
+                    break;
+
+            }
+
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Ingrese una opcion valida");
+            }
+
+        }
+
+        public void UpdateBooking(int bookingId) 
+        {
+        
+        }
+
+        /*
+        public BookingEntity GetBookingById(int bookingId)
+        {
+
+        }
+
+        public List<BookingEntity> GetBookingEntities()
+        {
+
+        }
+        */
+        public void DeleteBooking(int bookingId)
+        {
+        
+        }
+
     }
 }
--- Controllers/RentableEntityController.cs
+++ Controllers/RentableEntityController.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Booking.Models.Entities;
 
 namespace Boocking.Controllers
 {
     public class RentableEntityController
     {
+        
+        public void CreateEntity()
+        {
+
+        }
+
+        public void UpdateEntity(int entityId) 
+        { 
+        
+        }
+
+        /*
+        public RentableEntity GetEntityById(int entityId)
+        {
+            
+        }
+
+        public List<RentableEntity> GetEntities()
+        {
+
+        }
+        */
+
+        public void DeleteEntity(int entityId)
+        {
+
+        }
+
     }
 }
